bb959fde50aec452dbc0d98e56dbe4d6ccba9906
Muhammad Shahzad <abisim32@gmail.com>
2020-06-08 00:53:13 +0500

    Create Address.js

---
+++ src/Address.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Address (props){
+    return (
+    <div>
+        <h1>My Name is {props.name}.</h1>
+        <h2>My repository address is {props.url}</h2>
+    </div>
+    );
+}
+export default Address;
